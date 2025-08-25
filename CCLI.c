001516ae6dbf1b0b32344a0be40e8138713f462e
omerfuyar <denemeyo@gmail.com>
2025-08-25 13:41:59 +0300

    -works on local machine with 2 instances

--- CCLI.c
+++ CCLI.c
@@ -23,7 +23,10 @@ HOW TO USE
             <YOUR_COMPILER> CCLI.c -o CCLI
 
     -Run:
-        ./CCLI <ROOM_NAME / YOUR_NICK> <room / guest> <ROOM_PORT>
+        For Room:
+            ./CCLI <ROOM_NAME> room <ROOM_PORT>
+        For Guest:
+            ./CCLI <YOUR_NICK> guest <ROOM_PORT> <SERVER_IP_ADDRESS>
 */
 
 #include <stdio.h>
@@ -36,6 +39,7 @@ HOW TO USE
 #include <ws2tcpip.h>
 
 typedef unsigned int SocketHandle;
+typedef fd_set SocketSet;
 typedef struct sockaddr_in SocketAddress;
 
 #define SocketInitialize()                                                     \
@@ -61,7 +65,7 @@ typedef struct sockaddr_in SocketAddress;
         /*if ((*socketPtr = socket(AF_INET, SOCK_STREAM, isServer ? IPPROTO_TCP : 0)) == INVALID_SOCKET) */ \
         if ((*socketPtr = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)                               \
         {                                                                                                   \
-            printf("Socket creation failed : %d.\n", WSAGetLastError());                                    \
+            printf("Socket create failed : %d.\n", WSAGetLastError());                                      \
             SocketTerminate();                                                                              \
             exit(-1);                                                                                       \
         }                                                                                                   \
@@ -79,13 +83,13 @@ typedef struct sockaddr_in SocketAddress;
         }                                                             \
     } while (0)
 
-#define SocketSetOption(socketPtr, optLevel, optName)                                                         \
+#define SocketOption(socketPtr, optLevel, optName)                                                            \
     do                                                                                                        \
     {                                                                                                         \
         int option = 1;                                                                                       \
         if (setsockopt(*socketPtr, optLevel, optName, (const char *)&option, sizeof(option)) == SOCKET_ERROR) \
         {                                                                                                     \
-            printf("Socket option setting failed : %d.\n", WSAGetLastError());                                \
+            printf("Socket option set failed : %d.\n", WSAGetLastError());                                    \
             SocketClose(socketPtr);                                                                           \
             SocketTerminate();                                                                                \
             exit(-1);                                                                                         \
@@ -133,7 +137,7 @@ typedef struct sockaddr_in SocketAddress;
     {                                                                                                            \
         if (connect(*socketPtr, (struct sockaddr *)socketAddressPtr, sizeof(*socketAddressPtr)) == SOCKET_ERROR) \
         {                                                                                                        \
-            printf("Socket connection failed : %d.\n", WSAGetLastError());                                       \
+            printf("Socket connect failed : %d.\n", WSAGetLastError());                                          \
             SocketClose(socketPtr);                                                                              \
             SocketTerminate();                                                                                   \
             exit(-1);                                                                                            \
@@ -162,6 +166,17 @@ typedef struct sockaddr_in SocketAddress;
         }                                                                       \
     } while (0)
 
+#define SocketSetSelect(setPtr, setSize)                               \
+    do                                                                 \
+    {                                                                  \
+        if (select(0, setPtr, NULL, NULL, NULL) == SOCKET_ERROR)       \
+        {                                                              \
+            printf("Socket select failed : %d.\n", WSAGetLastError()); \
+            SocketTerminate();                                         \
+            exit(-1);                                                  \
+        }                                                              \
+    } while (0)
+
 #else // UNIX
 
 #include <arpa/inet.h>
@@ -170,6 +185,7 @@ typedef struct sockaddr_in SocketAddress;
 #include <unistd.h>
 
 typedef int SocketHandle;
+typedef fd_set SocketSet;
 typedef struct sockaddr_in SocketAddress;
 
 #define SocketInitialize()
@@ -181,7 +197,7 @@ typedef struct sockaddr_in SocketAddress;
     {                                                             \
         if ((*socketPtr = socket(AF_INET, SOCK_STREAM, 0)) == -1) \
         {                                                         \
-            perror("Socket creation failed");                     \
+            perror("Socket create failed");                       \
             SocketTerminate();                                    \
             exit(-1);                                             \
         }                                                         \
@@ -199,13 +215,13 @@ typedef struct sockaddr_in SocketAddress;
         }                                  \
     } while (0)
 
-#define SocketSetOption(socketPtr, optLevel, optName)                                               \
+#define SocketOption(socketPtr, optLevel, optName)                                                  \
     do                                                                                              \
     {                                                                                               \
         int option = 1;                                                                             \
         if (setsockopt(*socketPtr, optLevel, optName, (const char *)&option, sizeof(option)) == -1) \
         {                                                                                           \
-            perror("Socket option setting failed");                                                 \
+            perror("Socket option set failed");                                                     \
             SocketClose(socketPtr);                                                                 \
             SocketTerminate();                                                                      \
             exit(-1);                                                                               \
@@ -253,7 +269,7 @@ typedef struct sockaddr_in SocketAddress;
     {                                                                                                  \
         if (connect(*socketPtr, (struct sockaddr *)socketAddressPtr, sizeof(*socketAddressPtr)) == -1) \
         {                                                                                              \
-            perror("Socket connection failed");                                                        \
+            perror("Socket connect failed");                                                           \
             SocketClose(socketPtr);                                                                    \
             SocketTerminate();                                                                         \
             exit(-1);                                                                                  \
@@ -282,11 +298,17 @@ typedef struct sockaddr_in SocketAddress;
         }                                                     \
     } while (0)
 
+#define SocketSetSelect() \
+    do                    \
+    {                     \
+    } while (0)
+
 #endif
 
 #define USER_NICK_MAX_LENGTH 32
+#define MESSAGE_MAX_LENGTH 512
 #define ROOM_IP_MAX_LENGTH 128
-#define ROOM_MAX_GUEST_COUNT 5
+#define ROOM_MAX_GUEST_COUNT FD_SETSIZE
 
 typedef enum
 {
@@ -295,31 +317,15 @@ typedef enum
     UserModeRoom = 1
 } UserMode;
 
-char USER_NICK[USER_NICK_MAX_LENGTH];
+char USER_NICK[USER_NICK_MAX_LENGTH] = {0};
 UserMode USER_MODE = UserModeInvalid;
 short unsigned ROOM_PORT = 0;
 
 SocketAddress ROOM_ADDRESS = {0};
-SocketHandle USER_SOCKET = {0};
-SocketHandle ROOM_GUEST = {0};
+SocketHandle USER_SOCKET = {0}; // either room or guest (server or client)
 
 int main(const int argc, const char **argv)
 {
-    if (argc != 4)
-    {
-        printf("Invalid argument count. Read instructions in the source file.\n");
-        exit(-1);
-    }
-
-    unsigned char userNickLength = (unsigned char)strlen(argv[1]);
-    if (userNickLength > USER_NICK_MAX_LENGTH)
-    {
-        printf("User nick is too long. Max length must be %d.\n", USER_NICK_MAX_LENGTH);
-        exit(-1);
-    }
-
-    strncpy(USER_NICK, argv[1], userNickLength);
-
     if (strcmp("room", argv[2]) == 0)
     {
         USER_MODE = UserModeRoom;
@@ -334,6 +340,21 @@ int main(const int argc, const char **argv)
         exit(-1);
     }
 
+    if ((USER_MODE == UserModeRoom && argc != 4) || (USER_MODE == UserModeGuest && argc != 5))
+    {
+        printf("Invalid argument count for selected mode. Read instructions.\n");
+        exit(-1);
+    }
+
+    unsigned char userNickLength = (unsigned char)strlen(argv[1]);
+    if (userNickLength > USER_NICK_MAX_LENGTH)
+    {
+        printf("%s is too long. Max length must be %d.\n", USER_MODE == UserModeRoom ? "Room name" : "User nick", USER_NICK_MAX_LENGTH);
+        exit(-1);
+    }
+
+    strncpy(USER_NICK, argv[1], userNickLength);
+
     unsigned char roomPortLength = (unsigned char)strlen(argv[3]);
     if (roomPortLength > 5 && roomPortLength < 1)
     {
@@ -343,49 +364,113 @@ int main(const int argc, const char **argv)
 
     ROOM_PORT = (unsigned short)atoi(argv[3]);
 
-    printf("user nick : '%s'\n", USER_NICK);
-    printf("user mode : '%s'\n", argv[2]);
-    printf("room port : '%hu'\n", ROOM_PORT);
+    printf("\n");
 
     SocketInitialize();
 
     SocketCreate(&USER_SOCKET);
 
-    SocketSetOption(&USER_SOCKET, SOL_SOCKET, SO_REUSEADDR);
-
     ROOM_ADDRESS.sin_family = AF_INET;
     ROOM_ADDRESS.sin_port = htons(ROOM_PORT);
 
+    char messageBuffer[MESSAGE_MAX_LENGTH] = {0};
+
     if (USER_MODE == UserModeRoom) // user is room
     {
+        char hostName[256];
+        gethostname(hostName, sizeof(hostName));
+        printf("Server is running on host: %s\n", hostName);
+        printf("Potential room IPs to connect:\n");
+
+        struct hostent *pHostEntry = gethostbyname(hostName);
+        if (pHostEntry != NULL)
+        {
+            for (int i = 0; pHostEntry->h_addr_list[i] != NULL; i++)
+            {
+                struct in_addr addr;
+                memcpy(&addr, pHostEntry->h_addr_list[i], sizeof(struct in_addr));
+                printf("  - %s\n", inet_ntoa(addr));
+            }
+        }
+
         ROOM_ADDRESS.sin_addr.s_addr = INADDR_ANY;
 
+        SocketOption(&USER_SOCKET, SOL_SOCKET, SO_REUSEADDR);
+
         SocketBind(&USER_SOCKET, &ROOM_ADDRESS);
-        SocketListen(&USER_SOCKET, ROOM_MAX_GUEST_COUNT); // blocking
-        SocketAccept(&USER_SOCKET, &ROOM_GUEST);
+
+        SocketListen(&USER_SOCKET, ROOM_MAX_GUEST_COUNT);
+
+        SocketSet roomGuestSet = {0};
+        SocketSet roomGuestEventSet = {0};
+
+        FD_ZERO(&roomGuestSet);
+        FD_SET(USER_SOCKET, &roomGuestSet);
 
         while (1)
         {
-            char inputBuffer[255];
-            fgets(inputBuffer, sizeof(inputBuffer), stdin); // blocking
-            SocketSend(&ROOM_GUEST, inputBuffer, sizeof(inputBuffer));
+            roomGuestEventSet = roomGuestSet;
+
+            SocketSetSelect(&roomGuestEventSet, ROOM_MAX_GUEST_COUNT);
+
+            for (unsigned int index = 0; index < roomGuestEventSet.fd_count; index++)
+            {
+                SocketHandle triggeredSocket = roomGuestEventSet.fd_array[index];
+
+                if (FD_ISSET(triggeredSocket, &roomGuestEventSet))
+                {
+                    if (triggeredSocket == USER_SOCKET) // event from server, new connection
+                    {
+                        SocketHandle newGuestSocket = {0};
+                        SocketAccept(&USER_SOCKET, &newGuestSocket);
+                        FD_SET(newGuestSocket, &roomGuestSet);
+                    }
+                    else // event from already connected socket, handle connection
+                    {
+                        SocketReceive(&triggeredSocket, messageBuffer, sizeof(messageBuffer));
+
+                        if (strncmp(messageBuffer, "!q", 2) == 0)
+                        {
+                            SocketClose(&triggeredSocket);
+                            FD_CLR(triggeredSocket, &roomGuestSet);
+                        }
+                        else
+                        {
+                            printf("%s", messageBuffer);
+                            for (unsigned int i = 0; i < roomGuestSet.fd_count; i++)
+                            {
+                                SocketHandle targetSocket = roomGuestSet.fd_array[i];
+                                if (targetSocket != USER_SOCKET && targetSocket != triggeredSocket)
+                                {
+                                    SocketSend(&targetSocket, messageBuffer, sizeof(messageBuffer));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
     else // user is guest
     {
-        ROOM_ADDRESS.sin_addr.s_addr = inet_addr("127.0.0.1");
+        const char *serverIP = argv[4];
+        ROOM_ADDRESS.sin_addr.s_addr = inet_addr(serverIP);
 
         SocketConnect(&USER_SOCKET, &ROOM_ADDRESS);
 
         while (1)
         {
-            char messageBuffer[255];
-            SocketReceive(&USER_SOCKET, messageBuffer, sizeof(messageBuffer)); // blocking
-            printf("message received from room : %s\n", messageBuffer);
+            unsigned int nickOffset = userNickLength + sizeof("[] : ");
+
+            printf("[%s] : ", USER_NICK);
+            fgets((char *)messageBuffer + nickOffset, (int)(sizeof(messageBuffer) - nickOffset), stdin);
+            sprintf(messageBuffer, "[%s] : %s", argv[1], messageBuffer + nickOffset);
+            SocketSend(&USER_SOCKET, messageBuffer, sizeof(messageBuffer));
+
+            SocketReceive(&USER_SOCKET, messageBuffer, sizeof(messageBuffer));
+            printf("%s", messageBuffer);
         }
     }
-
-    SocketClose(&USER_SOCKET);
 
     SocketTerminate();
 }
